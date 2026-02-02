4ab46c160f74d9ac62284162d924ed8a2462928a
Aman Sudhakar <amansudhakar.cs25@bmsce.ac.in>
2026-02-02 22:11:58 +0530

    Update main.py
    added convo history to the bot

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,13 @@
 import os
+import asyncio
 from fastapi import FastAPI
 from pydantic import BaseModel
-from groq import Groq  #
+from groq import Groq  
 from dotenv import load_dotenv
 
 load_dotenv()
 
-# Initialize the Groq client
+#Initializing the Groq client
 client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
 
 app = FastAPI()
@@ -14,18 +15,25 @@ app = FastAPI()
 class ChatRequest(BaseModel):
     message: str
 
+chat_history = list()     #keeps a local history of the chat btw the bot
+
 @app.post("/chat")
 async def chat_endpoint(request: ChatRequest):
+    
     try:
-        # Create a completion using Llama 3
-        chat_completion = client.chat.completions.create(
-            messages=[
-                {"role": "user", "content": request.message}
-            ],
+
+        chat_history.append({"role" : "user", "content" : request.message})
+        chat_completion = await asyncio.to.thread ( 
+            
+            client.chat.completions.create,
+            messages = chat_history,
             model="llama-3.3-70b-versatile", # High-performance model
         )
         
-        # Return the response text
-        return {"response": chat_completion.choices[0].message.content}
+        ai_rep = chat_completion.choices[0].message.content
+        chat_history.append({"role" : "assistant", "content" : ai_rep})
+        return {"response": ai_rep}
+        
     except Exception as e:
+        
         return {"response": f"Groq API Error: {str(e)}"}
